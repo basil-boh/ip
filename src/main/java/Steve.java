e321b26597395db806ba0560f5a62877e6751c08
basil-boh <156693427+basil-boh@users.noreply.github.com>
2025-01-23 01:17:17 +0800

    - Added userData array storing up to 100 tasks - Returns list of tasks with command 'list'

--- src/main/java/Steve.java
+++ src/main/java/Steve.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Steve {
     public static void main(String[] args) {
+        String[] userData = new String[100];
+        int taskCount = 0;
         System.out.println("____________________________________________________________\n" +
                 "     Hello! I'm Steve\n" +
                 "     What can I do for you?\n" +
@@ -9,10 +11,22 @@ public class Steve {
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println("____________________________________________________________\n" +
-                    "     \n" + userInput + "\n" +
-                    "____________________________________________________________");
-            userInput = scanner.nextLine();
+            if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0,j =0; i < taskCount; i++) {
+                    System.out.println(++j + ": " + userData[i]);
+
+                }
+                System.out.println("____________________________________________________________");
+                userInput = scanner.nextLine();
+            }
+            else {
+                System.out.println("____________________________________________________________\n" +
+                        "     \n" + "added: " + userInput + "\n" +
+                        "____________________________________________________________");
+                userData[taskCount++] = userInput;
+                userInput = scanner.nextLine();
+            }
         }
         System.out.println("____________________________________________________________\n" +
                 "     \n" + "Bye! Hope to see you again soon =)" + "\n" +
