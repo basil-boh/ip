49b905b66e8bc93ebd4a3d21d131089a271ca6db
basil-boh <156693427+basil-boh@users.noreply.github.com>
2025-01-22 23:41:41 +0800

    - Renamed main class to Steve - Added simple static Greeting

---
+++ src/main/java/Steve.java
@@ -0,0 +1,10 @@
+public class Steve {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm Steve!\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
