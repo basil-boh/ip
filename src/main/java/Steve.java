06d260a0f54824962a61290235aa4f060c438b93
basil-boh <156693427+basil-boh@users.noreply.github.com>
2025-01-23 00:48:50 +0800

    - Added user input - Steve able to echo user inputs - Exits on 'bye' command

--- src/main/java/Steve.java
+++ src/main/java/Steve.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Steve {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Steve!\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+                "     Hello! I'm Steve\n" +
+                "     What can I do for you?\n" +
+                "____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println("____________________________________________________________\n" +
+                    "     \n" + userInput + "\n" +
+                    "____________________________________________________________");
+            userInput = scanner.nextLine();
+        }
+        System.out.println("____________________________________________________________\n" +
+                "     \n" + "Bye! Hope to see you again soon =)" + "\n" +
+                "____________________________________________________________");
     }
 }
