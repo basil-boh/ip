cc9a0593c09dc537b9eecf4eb056f88b59a3abcc
basil-boh <156693427+basil-boh@users.noreply.github.com>
2025-01-23 21:18:02 +0800

    - Added Exception Handling for cases: 1. InvalidTaskNumberException 2. NumberFormatException 3. emptyDescription
    - Added testcases for expected I/O

--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -11,4 +11,15 @@ class Messages {
                 "     \n" + "Bye! Hope to see you again soon =)" + "\n" +
                 "____________________________________________________________";
     }
+
+    public String unknown() {
+        String first = "Sorry I don't quite understand!";
+        String second = " Please add a task by entering: \n 1. todo \n 2. deadline \n 3. event \n";
+        String third = "Followed by a description of your task! =)";
+        return first + second + third;
+    }
+
+    public String descriptionEmpty() {
+        return "Description cannot be empty. Usage: todo <description>";
+    }
 }
--- src/main/java/Steve.java
+++ src/main/java/Steve.java
@@ -1,88 +1,115 @@
 import java.util.Scanner;
 
 public class Steve{
-    public static void main(String[] args) {
-        Messages message = new Messages();
-        Task[] userData = new Task[100];
-        int taskCount = 0;
-        System.out.println(message.greeting());
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        String restOfLine = "";
+    public static void main(String[] args) throws InvalidTaskNumberException {
+            Messages message = new Messages();
+            Task[] userData = new Task[100];
+            int taskCount = 0;
+            System.out.println(message.greeting());
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            String restOfLine = "";
 
-        while (!userInput.equals("bye")) {
-            String firstWord = userInput.split(" ")[0];
+            while (!userInput.equals("bye")) {
+                try {
+                    String firstWord = userInput.split(" ")[0];
 
-            switch (firstWord) {
-                case "list" -> {
-                    System.out.println("____________________________________________________________");
-                    for (int i = 0, j = 0; i < taskCount; i++) {
-                        Task currTask = userData[i];
-                        System.out.println(++j + currTask.list());
+                    switch (firstWord) {
+                        case "list" -> {
+                            System.out.println("____________________________________________________________");
+                            for (int i = 0, j = 0; i < taskCount; i++) {
+                                Task currTask = userData[i];
+                                System.out.println(++j + currTask.list());
 
+                            }
+                            System.out.println("____________________________________________________________");
+                            userInput = scanner.nextLine();
+                        }
+                        case "mark" -> {
+                            int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                            if (taskNumber < 0 || taskNumber >= taskCount) {
+                                throw new InvalidTaskNumberException("Task number " + taskNumber + " is invalid. Please enter a number within Task Count Range: " + taskCount);
+                            }
+                            taskNumber--;
+                            userData[taskNumber].markAsDone();
+                            System.out.println(userData[taskNumber].message(firstWord));
+                            userInput = scanner.nextLine();
+                        }
+                        case "unmark" -> {
+                            int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                            if (taskNumber < 0 || taskNumber >= taskCount) {
+                                throw new InvalidTaskNumberException("Task number " + taskNumber + " is invalid. Please enter a number within Task Count Range: " + taskCount);
+                            }
+                            taskNumber--;
+                            userData[taskNumber].markAsUndone();
+                            System.out.println(userData[taskNumber].message(firstWord));
+                            userInput = scanner.nextLine();
+                        }
+                        case "todo" -> {
+                            Scanner lineScanner = new Scanner(userInput);
+                            lineScanner.next();
+                            if (lineScanner.hasNextLine()) {
+                                restOfLine = lineScanner.nextLine().trim();
+                            }
+                            if (restOfLine.isEmpty()) {
+                                System.out.println(message.descriptionEmpty());
+                                userInput = scanner.nextLine();
+                                continue;
+                            }
+                            ToDo t = new ToDo(restOfLine);
+                            System.out.println(t.message());
+                            userData[taskCount++] = t;
+                            userInput = scanner.nextLine();
+                        }
+
+                        case "deadline" -> {
+                            Scanner lineScanner = new Scanner(userInput);
+                            lineScanner.next();
+                            if (lineScanner.hasNextLine()) {
+                                restOfLine = lineScanner.nextLine().trim();
+                            }
+                            if (restOfLine.isEmpty()) {
+                                System.out.println(message.descriptionEmpty());
+                                userInput = scanner.nextLine();
+                                continue;
+                            }
+                            Deadline d = new Deadline(restOfLine);
+                            System.out.println(d.message());
+                            userData[taskCount++] = d;
+                            userInput = scanner.nextLine();
+                        }
+
+                        case "event" -> {
+                            Scanner lineScanner = new Scanner(userInput);
+                            lineScanner.next();
+                            if (lineScanner.hasNextLine()) {
+                                restOfLine = lineScanner.nextLine().trim(); // Read the rest of the line
+                            }
+                            if (restOfLine.isEmpty()) {
+                                System.out.println(message.descriptionEmpty());
+                                userInput = scanner.nextLine();
+                                continue;
+                            }
+                            Event e = new Event(restOfLine);
+                            System.out.println(e.message());
+                            userData[taskCount++] = e;
+                            userInput = scanner.nextLine();
+                        }
+                        default -> {
+                            System.out.println(message.unknown());
+                            userInput = scanner.nextLine();
+                        }
                     }
-                    System.out.println("____________________________________________________________");
+                }
+                catch (InvalidTaskNumberException e) { //E.g. Catches invalid inputs like entering number 0 < or > taskCount
+                    System.out.println(e.getMessage());
                     userInput = scanner.nextLine();
                 }
-                case "mark" -> {
-                    int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-                    taskNumber--;
-                    userData[taskNumber].markAsDone();
-                    System.out.println(userData[taskNumber].message(firstWord));
-                    userInput = scanner.nextLine();
-                }
-                case "unmark" -> {
-                    int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-                    taskNumber--;
-                    userData[taskNumber].markAsUndone();
-                    System.out.println(userData[taskNumber].message(firstWord));
-                    userInput = scanner.nextLine();
-                }
-                case "todo" -> {
-                    Scanner lineScanner = new Scanner(userInput);
-                    lineScanner.next();
-                    if (lineScanner.hasNextLine()) {
-                        restOfLine = lineScanner.nextLine().trim();
-                    }
-                    ToDo t = new ToDo(restOfLine);
-                    System.out.println(t.message());
-                    userData[taskCount++] = t;
-                    userInput = scanner.nextLine();
-                }
-
-                case "deadline" -> {
-                    Scanner lineScanner = new Scanner(userInput);
-                    lineScanner.next();
-                    if (lineScanner.hasNextLine()) {
-                        restOfLine = lineScanner.nextLine().trim();
-                    }
-                    Deadline d = new Deadline(restOfLine);
-                    System.out.println(d.message());
-                    userData[taskCount++] = d;
-                    userInput = scanner.nextLine();
-                }
-
-                case "event" -> {
-                    Scanner lineScanner = new Scanner(userInput);
-                    lineScanner.next();
-                    if (lineScanner.hasNextLine()) {
-                        restOfLine = lineScanner.nextLine().trim(); // Read the rest of the line
-                    }
-                    Event e = new Event(restOfLine);
-                    System.out.println(e.message());
-                    userData[taskCount++] = e;
-                    userInput = scanner.nextLine();
-                }
-                default -> {
-                    Task t = new Task(userInput);
-                    System.out.println("____________________________________________________________\n" +
-                            "     \n" + "added: " + t.getDescription() + "\n" +
-                            "____________________________________________________________");
-                    userData[taskCount++] = t;
+                catch (NumberFormatException e) { //E.g. Catches invalid inputs like mark/unmark abc
+                    System.out.println("Please enter a valid number for the task!");
                     userInput = scanner.nextLine();
                 }
             }
-        }
-        System.out.println(message.goodbye());
+            System.out.println(message.goodbye());
     }
 }
