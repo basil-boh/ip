0473b005c5e13ff28ae37efdb8b471f8bf837c0c
basil-boh <156693427+basil-boh@users.noreply.github.com>
2025-01-24 15:39:14 +0800

    - Added comments for readability

--- src/main/java/Steve.java
+++ src/main/java/Steve.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Steve{
     public static void main(String[] args) {
+        // Initialising objects & variables 
             Messages message = new Messages();
             ArrayList<Task> userData = new ArrayList<>();
             message.greeting();
@@ -11,54 +12,59 @@ public class Steve{
             String restOfLine;
             CommandType command;
 
-            while (!userInput.equals("bye")) {
+            while (!userInput.equals("bye")) { //Exits on user command bye
                 try {
                     String firstWord = userInput.split(" ")[0];
 
-                    try {
+                    try { //Detects command (first word) -> assigns command as "unknown" if not a valid command
                         command = CommandType.valueOf(firstWord);
                     } catch (IllegalArgumentException e) {
                         command = CommandType.unknown;
                     }
 
-                    switch (command) {
+                    switch (command) { //Command: list
                         case list -> {
-                            printList(message, userData);
+                            printList(message, userData); //Prints out list of tasks followed by next user command
                             userInput = scanner.nextLine();
                         }
-                        case mark, unmark-> {
+                        case mark, unmark-> { //Command: mark/unmark
                             int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                            //Validates marking/unmarking of task & proceeds if valid
                             Task.validateTaskNumberMark(taskNumber, userData.size(), userData, command, firstWord);
                             userInput = scanner.nextLine();
                         }
-                        case todo -> {
+                        case todo -> { //Command: todo
+                            //Reads rest of line after command keyword
                             restOfLine = userInput.substring(firstWord.length()).trim();
-                            ToDo t = new ToDo(restOfLine);
-                            t.message();
-                            addTask(userData, t);
+                            ToDo t = new ToDo(restOfLine); //Creates todo object with using description
+                            t.message(); //Prints object message
+                            addTask(userData, t); //Adds task to Task ArrayList
                             userInput = scanner.nextLine();
                         }
                         case deadline -> {
+                            //Reads rest of line after command keyword
                             restOfLine = userInput.substring(firstWord.length()).trim();
-                            Deadline d = new Deadline(restOfLine);
-                            d.message();
-                            addTask(userData, d);
+                            Deadline d = new Deadline(restOfLine); //Creates deadline object with using description
+                            d.message(); //Prints object message
+                            addTask(userData, d); //Adds tasks to Task ArrayList
                             userInput = scanner.nextLine();
                         }
                         case event -> {
+                            //Reads rest of line after command keyword
                             restOfLine = userInput.substring(firstWord.length()).trim();
-                            Event e = new Event(restOfLine);
-                            e.message();
-                            addTask(userData, e);
+                            Event e = new Event(restOfLine); //Creates deadline object with using description
+                            e.message(); //Prints object message
+                            addTask(userData, e); //Prints object message
                             userInput = scanner.nextLine();
                         }
-                        case delete -> {
+                        case delete -> { //Command: delete
                             int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                            //Validates deletion of task & proceeds if valid
                             Task.validateTaskNumberDel(taskNumber, userData.size(), userData);
                             userInput = scanner.nextLine();
                         }
-                        case unknown -> {
-                            message.unknown();
+                        case unknown -> { //Command: unknown
+                            message.unknown(); //Prints invalid command message & takes in next user command
                             userInput = scanner.nextLine();
                         }
                     }
